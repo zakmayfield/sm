021d67ed464236a908d7b0874efe5619d12e1a8e
Zak Mayfield <zakmayfield@gmail.com>
2023-08-24 15:26:44 -0400

    feat: user form that uses next auth google sign in

--- src/features/user-auth-form/UserAuthForm.tsx
+++ src/features/user-auth-form/UserAuthForm.tsx
@@ -1,3 +1,23 @@
+'use client';
+import { signIn } from 'next-auth/react';
+import { useState } from 'react';
+
 export default function UserAuthForm() {
-  return <div>User auth form</div>;
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const loginWithGoogle = async () => {
+    setIsLoading(true);
+    try {
+      await signIn('google');
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={loginWithGoogle}>Google</button>
+    </div>
+  );
 }
