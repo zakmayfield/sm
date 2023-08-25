78c5d1b5d7577fb5531ba1de5b09f9e5cd958bd7
Zak Mayfield <zakmayfield@gmail.com>
2023-08-25 12:46:18 -0400

    chore: JWT cb returns jwt with custom info

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -57,57 +57,17 @@ export const authOptions: NextAuthOptions = {
         });
       }
 
-      return token;
+      return {
+        id: dbUser.id,
+        email: dbUser.email,
+        name: dbUser.name,
+        picture: dbUser.image,
+        username: dbUser.username,
+      };
+    },
+
+    redirect() {
+      return '/ ';
     },
   },
-  // callbacks: {
-  //   async session({ token, session }) {
-  //     if (token) {
-  //       session.user.id = token.id;
-  //       session.user.name = token.name;
-  //       session.user.email = token.email;
-  //       session.user.image = token.picture;
-  //       session.user.username = token.username;
-  //     }
-
-  //     return session;
-  //   },
-
-  //   async jwt({ token, user }) {
-  //     const dbUser = await db.user.findFirst({
-  //       where: {
-  //         email: token.email,
-  //       },
-  //     });
-
-  //     if (!dbUser) {
-  //       token.id = user!.id;
-  //       return token;
-  //     }
-
-  //     if (!dbUser.username) {
-  //       await db.user.update({
-  //         where: {
-  //           id: dbUser.id,
-  //         },
-  //         data: {
-  //           username: nanoid(10),
-  //         },
-  //       });
-  //     }
-
-  //     return {
-  //       id: dbUser.id,
-  //       name: dbUser.name,
-  //       email: dbUser.email,
-  //       picture: dbUser.image,
-  //       username: dbUser.username,
-  //     };
-  //   },
-  //   redirect() {
-  //     return '/';
-  //   },
-  // },
 };
-
-export const getAuthSession = () => getServerSession(authOptions);
